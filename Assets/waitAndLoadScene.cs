62d8093ba413da0511a8fd78edbbdc74c8f0157d
DESKTOP-MSG9PH7\Shivam <shivam.gupta@varaunited.com>
2017-12-03 03:00:52 +0530

    second commit

---
+++ Assets/waitAndLoadScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class waitAndLoadScene : MonoBehaviour
+{
+    public string sceneToLoad;
+    public int durationToWait;
+
+    void Start()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(durationToWait);
+        print(Time.time);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
